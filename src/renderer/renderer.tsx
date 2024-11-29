48c18de7b7aecab19f74c55a43fa35cbdca0deca
moonhr <moonhr153@gmail.com>
2024-11-29 17:21:55 +0900

    refactor: React StrictMode 및 App 컴포넌트 분리
    - `renderer.tsx` 파일에서 React.StrictMode를 추가하여 렌더링 안정성 강화.
    - App 컴포넌트를 별도의 파일로 분리하여 모듈화를 개선.
    - 기존의 inline App 정의를 제거하고, import 방식으로 변경.
    - 코드 가독성과 유지보수를 위한 구조 개선.

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,22 +1,13 @@
 // * 렌더러 프로세스 진입점
-
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
-const App = () => {
-  // IPC 통신 예시
-  const sendMessage = () => {
-    window.Electron.ipcRenderer.send("channel", "Hello from Renderer!");
-  };
+const container = document.getElementById("root");
+const root = createRoot(container!);
 
-  return (
-    <div>
-      <h1>Hello, Electron!</h1>
-      <button onClick={sendMessage}>Send Message</button>
-    </div>
-  );
-};
-
-const container = document.getElementById("root"); // 'root' ID를 가진 DOM 요소를 선택합니다.
-const root = createRoot(container!); // 선택한 DOM 요소를 기반으로 React 루트를 생성합니다.
-root.render(<App />); // 생성한 루트에 App 컴포넌트를 렌더링합니다.
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
