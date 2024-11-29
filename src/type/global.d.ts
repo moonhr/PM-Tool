9bcfb67f9cbba1f09c87bd376c378628bc54a501
moonhr <moonhr153@gmail.com>
2024-11-29 17:19:44 +0900

    fix |  글로벌 타입 정의 및 Electron 프리로드 수정 	•	global.d.ts 파일에 Electron 객체와 관련된 IPC 메서드의 타입 정의를 추가. 	•	saveRepositories, loadRepositories, openDirectoryDialog 메서드에 대한 구체적인 주석 추가. 	•	IPC 메서드 호출 시 타입 안전성을 보장하기 위한 리팩토링 진행.

--- src/type/global.d.ts
+++ src/type/global.d.ts
@@ -1,14 +1,26 @@
-// src/types/global.d.ts
+interface Repository {
+  id: string;
+  path: string;
+}
 declare global {
   interface Window {
     Electron: {
-      ipcRenderer: {
-        send: (channel: string, data: any) => void;
-        on: (
-          channel: string,
-          listener: (event: any, ...args: any[]) => void
-        ) => void;
-      };
+      /**
+       * Save repositories to the storage.
+       * @param repositories - An array of repositories to save.
+       */
+      saveRepositories: (repositories: Repository[]) => Promise<void>;
+
+      /**
+       * Load repositories from the storage.
+       * @returns A promise that resolves to an array of repositories.
+       */
+      loadRepositories: () => Repository<any[]>;
+      /**
+       * 파일 탐색기를 열어 디렉토리를 선택하는 함수
+       * @returns 선택한 디렉토리 경로 또는 null
+       */
+      openDirectoryDialog: () => Promise<string | null>;
     };
   }
 }
