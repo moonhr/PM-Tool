aec7ddd775d0f39c3c0bb3e9f8cfe37a22063fb4
moonhr <moonhr153@gmail.com>
2024-11-29 17:22:41 +0900

    refactor: App 컴포넌트 코드 정리 및 RepositoryManager 추가
    - `App.tsx`에서 React 컴포넌트를 리팩토링하여 코드 가독성 개선.
    - `RepositoryManager` 컴포넌트를 추가하고 렌더링되도록 연결.
    - App 컴포넌트를 `<div>`와 `<RepositoryManager />`로 구성하여 모듈화 강화.

---
+++ src/renderer/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RepositoryManager from "./components/RepositoryManager";
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello, Electron!</h1>
+      </div>
+      <RepositoryManager />
+    </>
+  );
+};
